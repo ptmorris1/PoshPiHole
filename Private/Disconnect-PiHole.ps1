9805241a0f392f64f3f6d17485ca6191c533690e
Patrick Morris <patrickthomasmorris@gmail.com>
2025-05-10 14:53:30 -0500

    updated disconnect-pihole

--- Private/Disconnect-PiHole.ps1
+++ Private/Disconnect-PiHole.ps1
@@ -1,22 +1,29 @@
 function Disconnect-PiHole {
     <#
     .SYNOPSIS
-    Ends the current Pi-hole session by its Session ID (SID).
+    Ends the current Pi-hole session by its ID or IDs.
 
     .DESCRIPTION
-    The function sends a DELETE request to the Pi-hole API to delete the session by ID.
+    The function sends a DELETE request to the Pi-hole API to delete the session(s) by ID.
 
     .PARAMETER BaseUrl
     The base URL of the Pi-hole instance (e.g., https://pi.hole).
 
     .PARAMETER Id
-    The ID of the session to be deleted.
+    The ID or array of IDs of the session(s) to be deleted.
 
+    .PARAMETER SID
+    The Session ID token required for authentication.
 
     .EXAMPLE
-    Disconnect-PiHole -BaseUrl "https://pi.hole" -SessionId 3 -Credential (Get-Credential)
+    Disconnect-PiHole -BaseUrl "https://pi.hole" -Id 3 -SID "abc123xyz"
 
-    This example deletes the session with the given SID from the specified Pi-hole instance using the provided credentials.
+    This example deletes the session with ID 3 from the specified Pi-hole instance using the provided session ID.
+
+    .EXAMPLE
+    Disconnect-PiHole -BaseUrl "https://pi.hole" -Id 3,4,5 -SID "abc123xyz"
+
+    This example deletes sessions with IDs 3, 4, and 5 from the specified Pi-hole instance using the provided session ID.
     #>
 
     param (
@@ -24,22 +31,21 @@ function Disconnect-PiHole {
         [string]$BaseUrl,
 
         [Parameter(Mandatory = $true)]
-        [string]$Id,
+        [string[]]$Id,
 
         [Parameter(Mandatory = $true)]
         [string]$SID
     )
 
-    $url = "$BaseUrl/api/auth/session/$Id"
-
-    # Create headers
-    $headers = @{
-        'X-FTL-SID' = $SID
-    }
-
-    try {
-        Invoke-RestMethod -Uri $url -Method Delete -Headers $headers -ErrorAction Stop
-    } catch {
-        Write-Host "Failed to delete session: $_" -ForegroundColor Red
+    foreach ($i in $Id) {
+        $url = "$BaseUrl/api/auth/session/$i"
+        $headers = @{
+            'X-FTL-SID' = $SID
+        }
+        try {
+            Invoke-RestMethod -Uri $url -Method Delete -Headers $headers -ErrorAction Stop
+        } catch {
+            Write-Host "Failed to delete session $i - $_" -ForegroundColor Red
+        }
     }
 }
